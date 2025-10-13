319c27b228769ed7f86ad3420d4f130cc0f81623
Amalrj <amalrajks6@gmail.com>
2025-10-13 22:54:14 +0530

    front end initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className="p-10 bg-red-500 text-white text-3xl font-bold">
+      🚀 Tailwind is Working!
+    </div>
+    </div>
+  )
+}
+
+export default App
