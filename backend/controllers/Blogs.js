3af30367461a577f90e25e74efac1b9ecc29e568
Amalrj <amalrajks6@gmail.com>
2025-10-13 22:39:31 +0530

    added the blogs update and delete

--- backend/controllers/Blogs.js
+++ backend/controllers/Blogs.js
@@ -1,4 +1,6 @@
 import PostModel from "../models/Blogs.js"
+import fs from 'fs'
+import path from 'path'
 
 const Create = async (req, res) => {
     try {
@@ -29,10 +31,17 @@ const DeleteBlog = async (req,res)=>{
     try{
  const id = req.query.id; // use query instead of params
         const deletedPost = await PostModel.findByIdAndDelete(id);        // const postId = await PostModel.find({})
-        console.log(id)
+
+
 
          if (!deletedPost) {
             return res.status(404).json({ success: false, message: "Post not found" });
+        }
+        if(deletedPost.image){
+            const profilePath = path.join('public/images',deletedPost.image)
+            fs.promises.unlink(profilePath)
+            .then(()=>{console.log('post deleted')})
+            .catch(err =>console.log(err))
         }
                 res.status(200).json({ success: true, message: "Post deleted", deletedPost });
 
@@ -44,4 +53,57 @@ const DeleteBlog = async (req,res)=>{
     }
 } 
 
-export { Create , DeleteBlog}
+const GetPost = async(req,res)=>{
+
+    try{
+        const posts = await PostModel.find({})
+        console.log(posts)
+
+        if(!posts){
+            return res.status(404).json({ success: false, message: "Post not found" });
+        }
+
+        res.status(200).json({success:true, message:posts})
+
+
+    }catch(err){
+        console.log(err)
+                res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+const UpdatePost = async(req,res)=>{
+    try{
+        const {title,description} = req.body
+    const postId = req.query.id
+    console.log(postId,'id')
+    const postUpdate = await PostModel.findById(postId)
+
+    console.log(postUpdate)
+    if(!postId){
+       return  res.status(404).json({success:false,message:'Post not found'})
+    }
+
+    if(title){
+        postUpdate.title = title
+    }
+    if(description){
+        postUpdate.description = description
+    }
+    if(req?.file){
+        postUpdate.image= req.file.filename
+    }
+
+    await postUpdate.save()
+    res.status(200).json({success:true,message:'post updated'})
+
+    }catch(err){
+        console.log(err)
+                        res.status(500).json({ success: false, message: "Internal Server Error" });
+
+    }
+
+
+}
+
+export { Create , DeleteBlog,GetPost,UpdatePost}
--- backend/routes/Blogs.js
+++ backend/routes/Blogs.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Create, DeleteBlog } from '../controllers/Blogs.js'
+import { Create, DeleteBlog, GetPost, UpdatePost } from '../controllers/Blogs.js'
 import { isAdmin } from '../middleware/IsAdmin.js'
 import upload from '../middleware/Multer.js'
 
@@ -7,6 +7,8 @@ import upload from '../middleware/Multer.js'
 const BlogsRoute = express.Router()
 BlogsRoute.post('/create',isAdmin,upload.single('image'), Create)
 BlogsRoute.delete('/delete',isAdmin,DeleteBlog)
+BlogsRoute.get('/getPost',GetPost)
+BlogsRoute.patch('/updatePost',isAdmin,upload.single('image'),UpdatePost)
 
 
 export default BlogsRoute
